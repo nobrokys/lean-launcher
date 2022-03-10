1ab0c374d49a94aeb33d52a97a921adc68a42a42
nobrokys <101305393+nobrokys@users.noreply.github.com>
2022-03-10 12:46:40 +1000

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -428,7 +428,7 @@ EzLauncher.NewsSection.ContentFrame.ScrollBarImageColor3 = Color3.fromRGB(112, 1
 EzLauncher.EzLauncher.Enabled = false;
 
 local launcherData = game:GetService("HttpService"):JSONDecode(
-	(game:HttpGet("https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/LauncherData.json")));
+	(game:HttpGet("https://raw.githubusercontent.com/nobrokys/lean-launcher/main/LauncherData.json")));
 
 EzLauncher.EzLauncher.Enabled = true;
 
